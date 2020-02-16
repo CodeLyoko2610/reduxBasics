d8844a5e917930684e6e2e2dec72e0a0b6c7e317
chuongcuu <dangchuong1999@outlook.com>
2020-02-16 21:59:59 +0200

    First reducer, test cases by expect package, prior ES6 syntax. | Add gitignore.

---
+++ index.js
@@ -0,0 +1,53 @@
+const expect = require('expect');
+
+function counter(state, action) {
+  if (typeof state === 'undefined') {
+    return 0;
+  }
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  }
+  return state;
+}
+
+expect(
+  counter(0, {
+    type: 'INCREMENT'
+  })
+).toEqual(1);
+
+expect(
+  counter(1, {
+    type: 'INCREMENT'
+  })
+).toEqual(2);
+
+expect(
+  counter(2, {
+    type: 'DECREMENT'
+  })
+).toEqual(1);
+
+expect(
+  counter(1, {
+    type: 'DECREMENT'
+  })
+).toEqual(0);
+
+//Unknown action
+expect(
+  counter(1, {
+    type: 'SOMETHING_ELSE'
+  })
+).toEqual(1);
+
+//Convention in Redux: If the received argument straight as type undefined, must return the intial state
+expect(
+  counter(undefined, {
+    type: 'INCREMENT'
+  })
+).toEqual(0);
+
+console.log('Tests passed!');
