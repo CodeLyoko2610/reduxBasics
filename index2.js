0b1331c3ca65ba9acf0c07accf39674aa8378088
chuongcuu <dangchuong1999@outlook.com>
2020-03-06 13:54:11 +0200

    Building createStore method from scratch | Tests to understand  Unsubscribe method.

--- index2.js
+++ index2.js
@@ -9,7 +9,7 @@ const counter = (state = 0, action) => {
       return state;
   }
 };
-
+//  -----------------------------------------CREATE STORE USING REDUX'S CREATESTORE FUNCTION--------------------------------------
 //Create Store
 const { createStore } = Redux; //Require redux-function via browser
 //const { createStore } = require('redux'); //Require redux-function when run in Node environment
@@ -28,5 +28,52 @@ render(); //calling render the first time to render the initial state 0
 
 document.addEventListener('click', () => {
   //2: dispatch method of the store
-  store.dispatch({ type: 'INCREMENT' });
+  store.dispatch({
+    type: 'INCREMENT'
+  });
 });
+
+//----------------------------------------------CREATE STORE FROM SCRATCH------------------------------------------------------------
+const createStoreChuong = reducer => {
+  let state;
+  let listeners = [];
+
+  const getState = () => {
+    return state;
+  };
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = listener => {
+    listeners.push(listener);
+
+    //Unsubscribe OTHER listeners
+    return () => {
+      listeners.filter(l => l !== listener);
+    };
+  };
+
+  //Dummy action, to get the initial state through the reducer
+  dispatch({});
+
+  return {
+    getState,
+    dispatch,
+    subscribe
+  };
+};
+
+// //For testing unsubscribe method
+// setInterval(() => {
+//   console.log('ahihi');
+//   store.dispatch({
+//     type: 'INCREMENT'
+//   });
+
+//   //Unsubscribe
+//   let unsubscribe = store.subscribe(render);
+//   unsubscribe();
+// }, 3000);
